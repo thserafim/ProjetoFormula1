8ca5fd3cede2c41ed79f297172a37235109a1aeb
thiagoseraffim@hotmail.com <thiagoseraffim@hotmail.com>
2025-06-06 14:37:09 -0300

    Estruturacao do projeto, criacao model. - dia 1

---
+++ F1telemetria/src/main/java/F1/Tdosoftware/F1telemetria/model/Lap.java
@@ -0,0 +1,14 @@
+package F1.Tdosoftware.F1telemetria.model;
+
+import lombok.Data;
+
+@Data
+public class Lap {
+
+    private String driverNumber;
+    private String lapTime;
+    private int lapNumber;
+
+
+
+}
