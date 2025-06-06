096ce5671e461d62c84adf0213d07d17ea392d42
thiagoseraffim@hotmail.com <thiagoseraffim@hotmail.com>
2025-06-06 13:04:32 -0300

    Estuturuando o projeto dia 1

---
+++ F1telemetria/src/main/java/F1/Tdosoftware/F1telemetria/F1telemetriaApplication.java
@@ -0,0 +1,13 @@
+package F1.Tdosoftware.F1telemetria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1telemetriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1telemetriaApplication.class, args);
+	}
+
+}
